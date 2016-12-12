bae022d23a1d80b35b42f7040a700f44b9360204
Alexander Kuzmin <roosit@abricos.org>
2016-12-12 09:32:11 +0300

    refactoring: bricks

--- src/includes/brick/categoryView.php
+++ src/includes/brick/categoryView.php
@@ -24,27 +24,20 @@ $isNew = isset($f[2]) && $f[2] == 'new';
 $app = Abricos::GetApp('blog');
 
 $cats = $app->CategoryList();
-
-
 $cat = $cats->GetByName($dir[1]);
-
-$vote = "";
 
 /** @var URatingApp $uratingApp */
 $uratingApp = Abricos::GetApp('urating');
+$voting = "";
 if (!empty($uratingApp) && !empty($cat->voting)){
-    $vote = $uratingApp->VotingHTML($cat->voting);
+    $voting = $uratingApp->VotingHTML($cat->voting);
 }
 
 $topics = $pa->topicList;
 
 $brick->content = Brick::ReplaceVarByData($brick->content, array(
-    'tl' => $cat->title,
-    "catname" => $cat->name,
-    "voting" => $vote,
+    'title' => $cat->title,
+    "voting" => $voting,
     'mbrs' => $cat->memberCount,
     'topics' => $cat->topicCount,
-    "newcnt" => $topics->totalNew > 0 ? "+".$topics->totalNew : "",
-    "f1sel" => !$isNew ? "sel" : "",
-    "f2sel" => !$isNew ? "" : "sel"
 ));
